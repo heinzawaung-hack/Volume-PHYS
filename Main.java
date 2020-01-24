1fa3b6b6faabc66fb9e38461552bf18905f3e803
heinzawaung-hack <55083887+heinzawaung-hack@users.noreply.github.com>
2020-01-24 09:35:44 +0630

    Only Source Code

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Cylinder cylinder = new Cylinder(3,5);
+        System.out.println("The area is " + cylinder.getVolume());
+
+    }
+}
+
+
